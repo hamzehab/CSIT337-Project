8a70a53065891af97ff3e9c846e480be13574548
Hamzeh Abdallah <hamzehhabdallah@gmail.com>
2022-11-25 01:55:12 -0500

    cart completed

--- Cart/actions.php
+++ Cart/actions.php
@@ -28,6 +28,7 @@
                     $_SESSION['cart'][$product_id]['quantity']++;
                 }
                 else{
+                    $item['productID'] = $product_id;
                     $item['categoryID'] = $product['categoryID'];
                     $item['productName'] = $product['productName'];
                     $item['productCode'] = $product['productCode'];
@@ -36,8 +37,94 @@
                     $_SESSION['cart'][$product_id] = $item;
                 }
                 header('location: view_cart.php');
-
             }
+            break;
+
+        case 'increase':
+            $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+            if ($product_id == NULL || $product_id == FALSE){
+                $error_message = "Product not found.";
+                include('../bootstrap.php');
+                echo "<title>Error</title>";
+                echo "<div class='container text-center p-3'><h1>Something went wrong! Product not found.</h1>";
+                echo "<a class='btn btn-dark m-3' href='../index.php'>Go Back</a></div>";
+            }
+            else{
+                if($_SESSION['cart'][$product_id]['quantity'] > 24) $_SESSION['cart'][$product_id]['quantity'] = 25;
+                else $_SESSION['cart'][$product_id]['quantity']++;
+                header('location: view_cart.php');
+            }
+
+            break;
+
+        case 'subtract':
+            $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+            if ($product_id == NULL || $product_id == FALSE){
+                $error_message = "Product not found.";
+                include('../bootstrap.php');
+                echo "<title>Error</title>";
+                echo "<div class='container text-center p-3'><h1>Something went wrong! Product not found.</h1>";
+                echo "<a class='btn btn-dark m-3' href='../index.php'>Go Back</a></div>";
+            }
+            else{
+                $_SESSION['cart'][$product_id]['quantity']--;
+                if ($_SESSION['cart'][$product_id]['quantity'] == 0) unset($_SESSION['cart'][$product_id]);
+                header('location: view_cart.php');
+            }
+            break;
+
+        case 'manual_entry':
+            $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+            $quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
+            if ($product_id == NULL || $product_id == FALSE){
+                $error_message = "Product not found.";
+                include('../bootstrap.php');
+                echo "<title>Error</title>";
+                echo "<div class='container text-center p-3'><h1>Something went wrong! Product not found.</h1>";
+                echo "<a class='btn btn-dark m-3' href='../index.php'>Go Back</a></div>";
+            }
+            else{
+                if ($quantity == 0){
+                    unset($_SESSION['cart'][$product_id]);
+                    header('location: view_cart.php');
+                }
+                else{
+                    if ($quantity > 25) $_SESSION['cart'][$product_id]['quantity'] = 25;
+                    else $_SESSION['cart'][$product_id]['quantity'] = $quantity;
+                    header('location: view_cart.php');
+                }
+            }
+            break;
+
+        case 'delete_item':
+            $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+            if ($product_id == NULL || $product_id == FALSE){
+                $error_message = "Product not found.";
+                include('../bootstrap.php');
+                echo "<title>Error</title>";
+                echo "<div class='container text-center p-3'><h1>Something went wrong! Product not found.</h1>";
+                echo "<a class='btn btn-dark m-3' href='../index.php'>Go Back</a></div>";
+            }
+            else{
+                unset($_SESSION['cart'][$product_id]);
+                header('location: view_cart.php');
+            }
+            break;
+
+        case 'order':
+            $street = trim(htmlspecialchars(filter_input(INPUT_POST, 'street')));
+            $street2 = trim(htmlspecialchars(filter_input(INPUT_POST, 'street2')));
+            $city = trim(htmlspecialchars(filter_input(INPUT_POST, 'city')));
+            $state = trim(htmlspecialchars(filter_input(INPUT_POST, 'state')));
+            $zipCode = trim(htmlspecialchars(filter_input(INPUT_POST, 'zipCode')));
+            if ($street == NULL || $street == FALSE || $city == NULL || $city == FALSE || $state == NULL || $state == FALSE || $zipCode == NULL || $zipCode == FALSE){
+                $error_message = "Product not found.";
+                include('../bootstrap.php');
+                echo "<title>Error</title>";
+                echo "<div class='container text-center p-3'><h1>Something went wrong! Product not found.</h1>";
+                echo "<a class='btn btn-dark m-3' href='../index.php'>Go Back</a></div>";
+            }
+            break;
     }
 
 ?>
--- Cart/view_cart.php
+++ Cart/view_cart.php
@@ -9,6 +9,11 @@
 
 <!DOCTYPE html>
 <html>
+    <script type="text/javascript">
+        function form_submit() {
+            document.getElementById("desc").click();
+        }    
+    </script>
     <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -46,25 +51,16 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 </ul>
                 <ul class="navbar-nav">
-                  <?php if (!isset($_SESSION['email'])){ ?>
                     <li class="nav-item">
-                          <a href="./Login/Login.php" class="nav-link" style="font-size: 1.2rem;">Login</a>
+                        <button class="btn btn-dark"><a href="../Login/Logout.php" class="nav-link"><i style="font-size: 1.5rem;" class="bi bi-box-arrow-right"></i>Logout</a></button>
                     </li>
-                    <li class="nav-item">
-                      <a href="./Login/Register.php" class="nav-link" style="font-size: 1.2rem;">Register</a>
-                    </li>
-                    <?php } else { ?>
-                      <li class="nav-item">
-                        <a href="./Login/Logout.php" class="nav-link"><i style="font-size: 1.5rem;" class="bi bi-box-arrow-right"></i>Logout</a>
-                      </li>
-                    <?php } ?>
                 </ul>
               </div>
             </div>
           </nav>
         </header>
         <title>Cart</title>
-        <section class="h-100 h-custom" style="background-color: #d2c9ff;">
+        <section class="h-100 h-custom" style="background-color: lightgrey;">
             <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12">
@@ -88,7 +84,9 @@
                             </div>
                             <hr class="my-4">
 
-                            <?php 
+                            <?php
+                                $count = 0;
+                                $total = 0; 
                                 if(isset($_SESSION['cart']) && count($_SESSION['cart']) > 0){
                                     foreach ($_SESSION['cart'] as $product): ?>
 
@@ -100,68 +98,136 @@
                                     <h6 class="text-muted"><?php echo get_category_name($product['categoryID']); ?></h6>
                                     <h6 class="text-black mb-0"><?php echo $product['productName']; ?></h6>
                                 </div>
-                                <div class="col-md-3 col-lg-3 col-xl-2 d-flex">
-                                <button class="btn btn-link px-2"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
-                                    <i class="bi bi-dash"></i>
-                                </button>
+                                <div class="col-xl-3 col-lg-3 col-xl-2 d-flex">
+                                    <form action="actions.php" method="POST">
+                                        <input type="hidden" name="action" value="subtract">
+                                        <input type="hidden" name="product_id" value="<?php echo $product['productID']; ?>">
+                                        <button class="btn btn-outline-dark mx-2 my-1"><i class="bi bi-dash"></i></button>
+                                    </form>
+                                    
+                                    <form action="actions.php" method="POST">
+                                        <input type="hidden" name="action" value="manual_entry">
+                                        <input type="hidden" name="product_id" value="<?php echo $product['productID']; ?>">
+                                        <input style="width:250%;" type="number" min="0" max="25" name="quantity" value="<?php echo $product['quantity']; ?>" class="form-control form-control-md text-center">
+                                    </form>
 
-                                <input id="form1" min="0" name="quantity" value="<?php echo $product['quantity']; ?>" type="number"
-                                    class="form-control form-control-sm" />
-
-                                <button class="btn btn-link px-2"
-                                    onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
-                                    <i class="bi bi-plus"></i>
-                                </button>
+                                    <form action="actions.php" method="POST">
+                                        <input type="hidden" name="action" value="increase">
+                                        <input type="hidden" name="product_id" value="<?php echo $product['productID']; ?>">
+                                        <button class="btn btn-outline-dark mx-5 my-1"><i class="bi bi-plus fa-lg"></i></button>
+                                    </form>
                                 </div>
                                 <div class="col-md-3 col-lg-2 col-xl-2 offset-lg-1">
-                                <h6 class="mb-0"><?php echo "$" . $product['quantity'] * $product['price']; ?></h6>
+                                    <h6 class="mb-0"><?php $individualTotal = $product['quantity'] * $product['price']; echo "$" . number_format($individualTotal, 2, '.', ' '); $total += $individualTotal; ?></h6>
                                 </div>
-                                <div class="col-md-1 col-lg-1 col-xl-1 text-end">
-                                <a href="#!" class="text-muted"><i class="fas fa-times"></i></a>
+
+                                <div class="col-md-1 col-lg-1 col-xl-1">
+                                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deleteModal<?php echo $count; ?>"><i class="bi bi-trash3"></i></button>
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="deleteModal<?php echo $count;?>" tabindex="-1" aria-labelledby="deleteModallabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-md">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h1 class="modal-title fs-5" id="deleteModalLabel">Remove item from cart?</h1>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <form action="actions.php" method="POST">
+                                                    <input type="hidden" name="action" value="delete_item">
+                                                    <input type="hidden" name="product_id" value="<?php echo $product['productID']; ?>">
+                                                    <div class="modal-body">
+                                                        <p>Are you sure you want to remove <?php echo $product['productName']; ?> from your cart?</p>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                        <button onclick="form_submit()" class='btn btn-danger' name="edit">Delete</a>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div> 
                                 </div>
                             </div>
 
                             <hr class="my-4">
-                            <?php endforeach; } ?>
+                            <?php $count++; endforeach; } ?>
 
                             <div class="pt-5">
-                                <h6 class="mb-0"><a href="../index.php" class="text-body"><i
-                                    class="bi bi-arrow-left me-1"></i>Continue Shopping</a></h6>
+                                <h6 class="mb-0"><a href="../index.php" class="text-body"><i class="bi bi-arrow-left me-1"></i>Continue Shopping</a></h6>
                             </div>
                             </div>
                         </div>
+                        
                         <div class="col-lg-4 bg-grey">
                             <div class="p-5">
                             <h3 class="fw-bold mb-5 mt-2 pt-1">Summary</h3>
                             <hr class="my-4">
 
                             <div class="d-flex justify-content-between mb-4">
-                                <h5 class="text-uppercase">items 3</h5>
-                                <h5>€ 132.00</h5>
+                                <h5>
+                                    <?php 
+                                    if (isset($_SESSION['cart'])){ 
+                                        echo count($_SESSION['cart']);
+                                        if(count($_SESSION['cart']) == 1) echo " item";
+                                        else echo " items";
+                                    }
+                                    ?>
+                                </h5>
+                                <h5>$<?php echo number_format($total, 2, '.'); ?></h5>
                             </div>
 
-                            <h5 class="text-uppercase mb-3">Shipping</h5>
+                            <div class="d-flex justify-content-between mb-5">
+                                <h5>Sales Tax</h5>
+                                <h5>$ <?php $tax = $total * .07; echo number_format($tax, 2, '.', ' '); ?></h5>
+                            </div>
 
                             <hr class="my-4">
 
                             <div class="d-flex justify-content-between mb-5">
-                                <h5 class="text-uppercase">Total price</h5>
-                                <h5>€ 137.00</h5>
+                                <h5>Total Price</h5>
+                                <h5>$ <?php $total *= 1.07; echo number_format($total, 2, '.', ' '); ?></h5>
                             </div>
-
-                            <button type="button" class="btn btn-dark btn-block btn-lg"
-                                data-mdb-ripple-color="dark">Place Order</button>
-
                             </div>
                         </div>
+                        <?php if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0){ ?>
+                        <div class="container">
+                            <h3 class="fw-bold mb-5 mt-2 pt-1">Shipping Information</h3>
+                            <form action="actions.php" method="POST">
+                                <input type="hidden" name="action" value="order">
+                                <div class="form-floating mb-3">
+                                    <input class="form-control" name="street" type="text" placeholder="Street Address" required>
+                                    <label for="floatingInput">Street Address</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input class="form-control" name="street2" type="email" placeholder="Street Address 2">
+                                    <label for="floatingInput">Street Address Line 2</label>
+                                </div>
+                                <div class="row">
+                                    <div class="col form-floating mb-3">
+                                        <input type="text" class="form-control" placeholder="City" name="city" required>
+                                        <label for="floatingInput">City</label>
+                                    </div>
+                                    <div class="col form-floating mb-3">
+                                        <input type="text" class="form-control" placeholder="State" name="state" required>
+                                        <label for="floatingInput">State / Province</label>
+                                    </div>
+                                    <div class="col form-floating mb-3">
+                                        <input type="text" class="form-control" placeholder="ZipCode" name="zipCode" required>
+                                        <label for="floatingInput">Postal / Zip Code</label>
+                                    </div>
+                                </div>
+                                <div class="col text-end p-3">
+                                    <button class="btn btn-success btn-lg" type="submit">Place Order</button>
+                                </div>
+                            </form>
+                        </div>
+                        <?php } ?>
                         </div>
                     </div>
                     </div>
                 </div>
                 </div>
             </div>
-            </section>
+        </section>
     </body>
 </html>
      
